56018a0aef83e548858e398776ec779a79a2a277
jiananWang <a0918666589@gmail.com>
2018-06-06 05:14:36 +0800

    feature: buy stock button

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="col-sm-12 col-md-6 col-lg-4 mb-2">
+    <div class="card mb-3">
+      <div class="card-header  bg-success  text-white">
+        <div>{{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
+        </div>
+      </div>
+      <div class="card-body d-flex justify-content-between">
+        <input type="number"
+        class="form-control form-style"
+        placeholder="Quantity"
+        v-model="quantity">
+        <button class="btn btn-success"
+        @click="buyStock"
+        :disabled="btnDisabled">Buy</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      console.log(order);
+      this.quantity = 0;
+    }
+  },
+  computed: {
+    btnDisabled() {
+      return this.quantity <= 0 || !Number.isInteger(Number(this.quantity));
+    }
+  }
+};
+</script>
+
+
+<style lang="scss">
+.form-style {
+  width: 70%;
+}
+</style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>The Stocks Page</h1>
+  <div class="mt-3 row">
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+  </div>
 </template>
+
+<script>
+import Stock from "./Stock";
+
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "Amazon", price: 1670 },
+        { id: 2, name: "Apple", price: 192 },
+        { id: 3, name: "Microsoft", price: 102 },
+        { id: 4, name: "Alphabet", price: 1137 }
+      ]
+    };
+  },
+  components: {
+    AppStock: Stock
+  }
+};
+</script>
